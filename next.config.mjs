0f29378c40a5169fbe46d2027e1a7c72f11c3454
iancampusano <34844163+iancampusano@users.noreply.github.com>
2025-02-17 18:16:07 -0300

    Fix error

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true, // Si estás usando la carpeta `app`
-  },
-};
-
-module.exports = nextConfig;
